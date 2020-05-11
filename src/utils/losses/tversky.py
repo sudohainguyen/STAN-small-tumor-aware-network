d3d9150afe23dbbd4d9a9d29d9d600fa269ed4d7
Hai Nguyen <quanghai.ng@outlook.com.vn>
2020-05-12 00:19:04 +0700

    tversky loss

--- src/utils/losses/tversky.py
+++ src/utils/losses/tversky.py
@@ -1,6 +1,29 @@
+# Copyright (c) 2020 Hai Nguyen
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+import tensorflow.keras.backend as K
+
+
+def _tversky_index(y_true, y_pred):
+    smooth = 1.0
+    y_true = K.flatten(y_true)
+    y_pred = K.flatten(y_pred)
+
+    tp = K.sum(y_true * y_pred)
+    fn = K.sum(y_true * (1 - y_pred))
+    fp = K.sum((1 - y_true) * y_pred)
+    alpha = 0.7
+
+    return (tp + smooth) / (tp + alpha * fn + (1 - alpha) * fp + smooth)
+
+
 def tversky_loss(y_true, y_pred):
-    pass
+    return 1 - _tversky_index(y_true, y_pred)
 
 
 def focal_tversky_loss(y_true, y_pred):
-    pass
+    pt_1 = _tversky_index(y_true, y_pred)
+    gamma = 0.75
+    return K.pow((1 - pt_1), gamma)
