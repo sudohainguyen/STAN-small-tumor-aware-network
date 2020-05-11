8779c79200e8a503847058cd11c16fababc05aaf
Hai Nguyen <quanghai.ng@outlook.com.vn>
2020-05-11 22:37:27 +0700

    remove old miou metric

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,18 +1,11 @@
+# Copyright (c) 2020 Hai Nguyen
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
-
-
-def mean_iou(y_true, y_pred):
-    prec = []
-    for t in np.arange(0.5, 1.0, 0.05):
-        y_pred_ = tf.to_int32(y_pred > t)
-        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
-        K.get_session().run(tf.local_variables_initializer())
-        with tf.control_dependencies([up_opt]):
-            score = tf.identity(score)
-        prec.append(score)
-    return K.mean(K.stack(prec), axis=0)
 
 
 def dice_coef(y_true, y_pred):
