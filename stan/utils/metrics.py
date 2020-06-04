be0d2b6a7cb310765360ec73d5870c5fb041c939
Hai Nguyen <quanghai.ng@outlook.com.vn>
2020-06-04 22:12:00 +0700

    update metrics

--- stan/utils/metrics.py
+++ stan/utils/metrics.py
@@ -3,13 +3,42 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-import numpy as np
-import tensorflow as tf
 import tensorflow.keras.backend as K
 
 
-def dice_coef(y_true, y_pred, smooth=1.0):
+def dice_coef(y_true, y_pred):
+    smooth = 1.
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    union = K.sum(y_true_f) + K.sum(y_pred_f)
+    return (2. * intersection + smooth) / (union + smooth)
+
+
+def true_pos(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    tp = (K.sum(y_pos * y_pred_pos) + smooth) / (K.sum(y_pos) + smooth) 
+    return tp 
+
+
+def true_neg(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pred_neg = 1 - y_pred_pos
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos
+    tn = K.sum(y_neg * y_pred_neg)
+    tn_ratio = (tn + smooth) / (K.sum(y_neg) + smooth)
+    return tn_ratio
+
+
+def false_pos(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos
+    fp = K.sum(y_neg * y_pred_pos)
+    fp_ratio = (fp + smooth) / (K.sum(y_neg) + smooth)
+    return fp_ratio
